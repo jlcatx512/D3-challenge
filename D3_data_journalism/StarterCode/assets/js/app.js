c64649cc9025b4fdd703d85e44533c9c346abe03
Jadd Cheng <52934066+jlcatx512@users.noreply.github.com>
2019-11-13 14:19:53 -0600

    Second commit. Main functionality of scatter plot is working. Need to fix dynamic interactivity code.

---
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -0,0 +1,310 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth + margin.left + margin.right)
+  .attr("height", svgHeight + margin.top + margin.bottom);
+// Add margins???
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty"; // poverty
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(censusData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.95, // give an offset 0.8,
+      d3.max(censusData, d => d[chosenXAxis]) * 1.05 // 1.2
+    ])
+    .range([0, width]);
+  return xLinearScale;
+}
+
+// function used for updating y-scale var upon click on axis label
+function yScale(censusData, chosenYAxis) {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d[chosenYAxis]) * 0.80, // 0.8,
+      d3.max(censusData, d => d[chosenYAxis]) * 1.2 // 1.2
+    ])
+    .range([height, 0]); // big difference with x. range is reversed.
+  return yLinearScale;
+}
+
+// creatre one for y
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+//   var leftAxis = d3.axisLeft(newYScale);
+  
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, newYScale, chosenXaxis, chosenYAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+    // .attr("cy", d=> newYScale(d[chosenYAxis]))
+    // new cy?
+    // need to update function, newYScale and new chosenYaxis.
+
+  return circlesGroup;
+}
+
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+    var label  = "";
+    if (chosenXAxis === "poverty") {
+        label = "Poverty %:";
+    }
+    else {
+        label = "Age (Median):";
+    }
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.poverty}<br>${label} ${d[chosenXAxis]}`);
+        });
+
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+    })
+
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+        toolTip.hide(data, this);
+    });
+
+  return circlesGroup;
+}
+
+
+// Retrieve data from the CSV file and execute everything below
+(async function(){
+    var censusData = await d3.csv("assets/data/data.csv");
+    console.log(censusData);
+
+    // parse data
+    // id,state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,healthcare,healthcareLow,healthcareHigh,
+    // obesity,obesityLow,obesityHigh,smokes,smokesLow,smokesHigh,-0.385218228
+    censusData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+        data.age = +data.age;
+        data.income = +data.income;
+        data.smokes = +data.smokes;
+        data.obesity = +data.obesity; 
+    });
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(censusData, chosenXAxis);
+
+    // fix this up top --> need to change to yScale function.
+    // Create y scale function
+    var yLinearScale = yScale(censusData, chosenYAxis)
+    // var yLinearScale = d3.scaleLinear()
+    //     .domain([d3.min(censusData, d => d[chosenYAxis]),
+    //     d3.max(censusData, d => d[chosenYAxis])])
+    //     // .domain([d3.extent(censusData, d => d[chosenYAxis])
+    //     .range([height, 0]);
+
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+        .classed("y-axis", true)
+        // .attr("transform", `translate(0, 0)`)
+        .call(leftAxis);
+
+    // append initial circles
+    // append a group?
+    /*
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("g")
+        .append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d[chosenYAxis]))
+        .attr("r", 15)
+        .classed("stateCircle", true)
+        .append("text")
+        .classed("my-text stateText", true)
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d[chosenXAxis]))
+        .attr("y", d => yLinearScale(d[chosenYAxis])+3.8);
+    */
+
+    // A group that will group both 1. circle shapes 2. text shapes
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("g")
+    
+    circlesGroup.append("circle")
+        .attr("cx", d => xLinearScale(d[chosenXAxis]))
+        .attr("cy", d => yLinearScale(d[chosenYAxis]))
+        .attr("r", 15)
+        .classed("stateCircle", true)
+    
+    // change this to circlesLables
+    // var circleLabels = chartGroup.selectAll("text").
+    // .data().
+    // .enter()
+    // .append
+    circlesGroup.append("text")
+        .classed("my-text stateText", true)
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d[chosenXAxis]))
+        .attr("y", d => yLinearScale(d[chosenYAxis])+4);
+        // .attr("fill", "#009294")
+        // .attr("opacity", ".40");
+/*    
+    // append text group
+    var textGroup = chartGroup.selectAll("text.my-text")
+        .data(censusData)
+        .enter()
+        .append("text")
+        .classed("my-text stateText", true)
+        .attr("x", d => xLinearScale(d[chosenXAxis]))
+        .attr("y", d => yLinearScale(d[chosenYAxis])+3.8)
+        .text(d => d.abbr);
+        // .style("fill", "white");
+*/
+    // Create group for  2 x- axis labels
+    var xLabelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var povertyLabel = xLabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "poverty") // value to grab for event listener
+        .classed("active aText", true)
+        .text("In Poverty (%)");
+
+    var ageLabel = xLabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "age") // value to grab for event listener
+        .classed("inactive", true)
+        .text("Age (Median)");
+        
+    var incomeLabel = xLabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 60)
+        .attr("value", "income") // value to grab for event listener
+        .classed("inactive", true)
+        .text("Household Income (Median)");
+
+    // append y axis
+    // var ylabelsGroup = chartGroup.append("g")
+    //     .attr("transform," `translate(${width / 2}, ${height + 20})`)
+    //     .classed("active", true);
+
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 5)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("axis-text", true)
+        .text("Lacks Healthcare %");
+
+    // updateToolTip function above csv import
+    circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+    // create y labels group
+    // x axis labels event listener
+    xLabelsGroup.selectAll("text")
+        .on("click", function() {
+        // get value of selection
+        var value = d3.select(this).attr("value");
+        console.log(value);
+
+        if (value !== chosenXAxis) {
+
+            // replaces chosenXAxis with value
+            chosenXAxis = value;
+
+            console.log("chosenXAxis is:" + chosenXAxis)
+
+            // functions here found above csv import
+            // updates x scale for new data
+            xLinearScale = xScale(censusData, chosenXAxis);
+
+            // updates x axis with transition
+            xAxis = renderAxes(xLinearScale, xAxis);
+
+            // updates circles with new x values
+            circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+            // updates tooltips with new info
+            circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+            // changes classes to change bold text
+            // need x and y labels updated too.
+            if (chosenXAxis === "poverty") {
+                povertyLabel
+                    .classed("active aText", true)
+                    .classed("inactive", false);
+                ageLabel
+                    .classed("active", false)
+                    .classed("inactive", true);
+                incomeLabel
+                    .classed("active", false)
+                    .classed("inactive", true);
+            }
+            else {
+                povertyLabel
+                    .classed("active", false)
+                    .classed("inactive", true);
+                ageLabel
+                    .classed("active aText", true)
+                    .classed("inactive", false);
+            }
+        }
+    });
+})()
--- README.md
+++ README.md
@@ -1,2 +1,154 @@
 # D3-challenge
 November 7, 2019
+
+## OVERVIEW ##
+* [Project repo](https://github.com/jlcatx512/D3-challenge)
+* [Instructions](https://github.com/the-Coding-Boot-Camp-at-UT/UT-MCB-DATA-PT-07-2019-U-C/tree/master/homework-instructions/16-D3/Instructions)
+
+## Debrief ##
+* Needed to literally refresh memory by redoing all videos.
+* Change everything to a `var`! Because it breaks scope.
+* NB import d3 tip js file.
+
+## ACTION ITEMS ##
+* [Dev Tools](https://developers.google.com/web/tools/chrome-devtools)
+    * Core Workflows
+    * > When you want to see logged messages or run JavaScript, press Command+Option+J (Mac) or Control+Shift+J (Windows, Linux, Chrome OS) to jump straight into the Console panel.
+    * [JS Debuggin Chrome DevTools](https://developers.google.com/web/tools/chrome-devtools/javascript/)
+* [UTAUS201901DATA3Wed, Nov 6 2019 at 6:18 PM](https://bootcampspot.com/sessions/591052/videos/62865)
+* https://bootcampspot.com/sessions/591045/videos/63375
+    *  Mon reviews text elements.
+    * Adding `text` tag to `svg` element.  
+    * Code for text should look almost identical to the circles.
+        * NB do NOT select text tag. `selectAll(".my-text")`
+    * `.append(text)` .. this appends the new `<text>` tag in the `chartGroup`.
+    * `.classed("my-text", true)` add class of my-text
+    * Fix x and y attributes.
+    * No radius
+    * No fill or opacity.
+    * `.text(d=>d.num_hits)`
+* ~~https://www.oreilly.com/learning/making-a-scatterplot-with-d3-js~~
+
+## Text Group sample code ##
+> `var textGroup = chartGroup.selectAll("text.my-text")
+>         .data(hairData)
+>         .enter()
+>         .append("text")
+>         .classed("my-text", true)
+>         .attr("x", d => xLinearScale(d[chosenXAxis]))
+>         .attr("y", d => yLinearScale(d.num_hits))
+>         .text(d => d.num_hits)`
+
+<hr>
+
+## Setting up local web server ##
+* On Windows
+* > `python -m http.server 8000 --bind 127.0.0.1`
+* > `python -m http.server 5000 --bind 127.0.0.1`
+* Try ports `5001` or `3000`.
+
+## Parse the data ##
+* data.csv
+    |variable| description | notes|
+    |--     |--             | --    |
+    | `id`  |   |
+    | `state`   |   |
+    | `abbr`    |   |
+    | `poverty`     |   |
+    | `povertyMoe`  |   |
+    | `age`     |   |
+    | `ageMoe`  |   |
+    | `income`  |   |
+    | `incomeMoe`   |   |
+    | `healthcare`  |   |
+    | `healthcareLow`   |   |
+    | `healthcareHigh`  |   |
+    | `obesity`     |   |
+    | `obesityLow`  |   |
+    | `obesityHigh`     |   |
+    | `smokes`  |   |
+    | `smokesLow`   |   |
+    | `smokesHigh`  |   |
+    | `-0.385218228`    |   | A mistake?
+
+## Axes ##
+* [Adding a simple guide From Learning Data Visualization with D3.js](https://www.lynda.com/D3-js-tutorials/Adding-simple-guide/594451/619574-4.html)
+* [D3 documentation](https://github.com/d3/d3-axis)
+
+<hr>
+
+## Data Join ##
+* Definition
+* [Lynda - D3 subselections](https://www.lynda.com/D3-js-tutorials/Creating-subselections/594451/619558-4.html)
+    * How to think about `enter()`. A bit of time travel.
+    * Acting on elements that don't exist yet.
+    * 1. `select` element that DOES exist.  | somethign there. |
+    * 2. `selectAll` elements that do NOT exist.    | somethign there. |
+    * 3. `data()` bind data to selection.
+    * 4. `enter()`  | somethign there. |
+    * 5. `append()` element in selectAll that do not exist. NB referred to twice!
+    * 6. `html()` and function. Act on the new data elements.
+* NB --> set up data as an array of objects. Objects are like dicts. 
+* > Using D3 you create a _____ for programmatically-created elements, and then queue up commands for these elements.
+
+## SVG Review ##
+* [Mozilla Reference SVG](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes)
+* [SVG Text Element](https://www.dashingd3js.com/svg-text-element)
+
+## Importance of scaling ##
+* Have the SVG shapes scale in size according to the size of the SVG canvas/container.
+* [How Scales Work - Data Visualization and D3.js(]https://www.youtube.com/watch?v=0hnrHnrXWx4)
+* [Generative Data Visualization in D3.js pt.2 (with Shirley Wu) — Learn With Jason](https://www.youtube.com/watch?v=S3TS6IGSUS4)
+* `domain` input
+* `range` output
+
+## Event handling in d3 ##
+* NB to target element with `this` keyword.
+* https://github.com/d3/d3/blob/master/API.md#handling-events
+* What element does it attach to?
+
+## d3 Transitions ##
+* Create a myChart `var`.
+* https://www.dashingd3js.com/lessons/d3-transition-basics
+
+## Scatter Plot Examples ##
+* [](https://www.d3-graph-gallery.com/scatter.html)
+
+## Circles and Labels ##
+* https://stackoverflow.com/questions/36954426/adding-label-on-a-d3-scatter-plot-circles
+* Create a group!
+* Add circles and texts separately to that group.
+
+## Tool Tips ##
+* NB DON'T FORGET TO ADD .js to source index.html!
+
+## d3 Axis ##
+* https://github.com/d3/d3-axis
+* http://bl.ocks.org/emmasaunders/cebb1837530c876def717c0e5c61da31
+
+## d3 color scales ##
+* http://bl.ocks.org/emmasaunders/52fa83767df27f1fc8b3ee2c6d372c74
+*  lynda.com/D3-js-tutorials/Making-tree-from-JSON/504428/549433-4.html
+
+## SVG `text` elements ##
+* `text-anchor` to align horizontally and `dominant-baseline` to align vertically. ALong default position of lower left-hand corner.
+* The x and y default position behavior of text element is the left hand bottom.
+* To change where that x, y coordinate is, you need to use text-anchor and dominant-baseline. You can do this with attr or CSS.
+* https://www.lynda.com/D3-js-tutorials/Adding-text/504428/549404-4.html/
+> - [Instructor] In theory, all you need to add text to your SVG is the text itself because browsers default thex and y coordinates to zero. 
+* debugging --> click on DOM. find your element. if you click on it while in the inspector, you should see something on the page. 
+* Three ways to style svg text elements.
+    1. `attr`
+        *  e.g. attr(") fill stroke  
+        * text is a shape
+        * `fill`
+        * `stroke`
+        * `stroke-width`
+    2. `style`
+    3. CSS
+* `dominant-baseline` --> `middle`
+* https://bl.ocks.org/emmasaunders/0016ee0a2cab25a643ee9bd4855d3464
+    * --> version3
+* tspan --> to add a new line. Do a data join with tspan elements.
+* selectAll("tspan").data(array).enter().append("tspan")
+* https://www.dashingd3js.com/lessons/svg-text-elements
